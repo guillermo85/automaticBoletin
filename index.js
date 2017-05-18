7044efea1e2e5b01a0ed40bc12da4ec4443c6c82
Guillermo <guillermo85@gmail.com>
2017-05-18 14:02:59 -0300

    Emprolijo codigo y agego comentarios

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+/******************************
+Filtra vulnerabilidades de nist del ultimo mes y con riesgo medio-alto.
+Version 5/2017
+Autor: Guillermo
+*******************************/
+
 //Carga de dependencias
 const fs = require('fs');
 const download = require('download');
 const decompress = require('decompress');
-//var https   = require('https');
-//var unzip = require('unzip');
 var moment = require('moment');
 moment().format();
+//var https   = require('https');
+//var unzip = require('unzip');
 
-//Definicion de variables
+//Definicion de variables globales
 var url = "https://static.nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-modified.json.zip";
+var json1 = JSON.parse(fs.readFileSync('cve.json', 'utf8'));
+var array = JSON.stringify(json1.CVE_Items);
+var itera = 0;
+var largo = json1.length;
+var addResults = [];
+
 //Inicio descarga de archivo
 /*
 download(url, 'vulns').then(() => {
@@ -20,62 +32,44 @@ download(url, 'vulns').then(() => {
 fs.createReadStream('./vulns/cve.zip').pipe(unzip.Extract({ path: './vulns' }));
 */
 
-var json1 = JSON.parse(fs.readFileSync('cve.json', 'utf8'));
-var array = JSON.stringify(json1.CVE_Items);
-var itera = 0;
-var largo = json1.length;
-
+//Del Json cargado desde el archivo en la variable json1, leo todos los objetos uno por uno en item:
 for (var item of json1.CVE_Items) {
-    //console.log('item: ', JSON.stringify(item));
-    //Utilizar moment.js para comparar fechas
-    //var str = '04/17/2017';
-	//var res = str.slice(0, 2);
-	//console.log(res);
-
-	itera = itera + 1;
-    if (typeof item.CVE_references.CVE_reference_data[0] !== 'undefined'){
+    itera = itera + 1; //Para conocer numero de objeto dentro del array
+    if (typeof item.CVE_references.CVE_reference_data[0] !== 'undefined'){ //Para que no de error de undefined al leer el valor
      	if (item.CVE_references.CVE_reference_data[0].publish_date ){
-     		var date = JSON.stringify(item.CVE_references.CVE_reference_data[0].publish_date);
-     		//console.log('Fecha:', date);
-     		var day = date.slice(1, 3), month = date.slice(4, 6), year = date.slice(7, 11);
-     		//console.log('Fecha:', day + month + year );
-
+     		var date = JSON.stringify(item.CVE_references.CVE_reference_data[0].publish_date); //Capturo fecha en formato dd/mm/yyyy
+     		var day = date.slice(1, 3), month = date.slice(4, 6), year = date.slice(7, 11); //Capturo cada dato de la fecha por separado
+     		//Utilizo moment.js para comparar fechas
      		var now = moment();
-     		var publishDate = moment(year+'-'+month+'-'+day);
-     		var howFar = publishDate.fromNow(true);
-     		//console.log(howFar);
-		    
-		    var substring2 = "a month";
+     		var publishDate = moment(year+'-'+month+'-'+day); //Lo paso al formato que acepta moment.js para comparar fechas
+     		var howFar = publishDate.fromNow(true); //Le pregunto a moment.js cuantos dias/meses pasaron desde la fecha en cuestion
+     		var substring2 = "a month";
 		    if ( howFar.indexOf(substring2) > -1 ){
-		    	//console.log(itera + '2 ' + howFar);
 		    	// LLAMAR A FUNCION FILTRO DE RIESGO (risk_filter) pasando el dato del numero de iteracion
 		    	if( typeof item.CVE_impact.CVE_impact_cvssv3.bm !== 'undefined' ){
 		    		if (item.CVE_impact.CVE_impact_cvssv3.bm.score > 4){
-		    			console.log('Riesgo: ', JSON.stringify(json1.CVE_Items[itera]));
-		    			//console.log('Riesgo:', JSON.stringify(item.CVE_impact.CVE_impact_cvssv3.bm.score));
+		    			addResults.push(json1.CVE_Items[itera]); //Cargo los resultados en el array addResults que sera el mostrado al final como resultado
 		    		}     	
 		    	}
-		    	//json1.splice(itera,1);
-		    	/*if ( json1.CVE_Items[itera] ){ 
-		    		console.log('esta');
-		    	    //array.splice(itera,1);
-		    	    //console.log('4 ' + json1.CVE_Items[itera]);
-		    	}*/
 		    }
 		    var substring2 = "days";
 		    if ( howFar.indexOf(substring2) > -1 ){
-		    	//console.log(itera + '3 ' + howFar);
 		    	console.log('Total: ', JSON.stringify(json1.CVE_Items[itera]));
 		    	// LLAMAR A FUNCION FILTRO DE RIESGO (risk_filter) pasando el dato del numero de iteracion
 		    	if( typeof item.CVE_impact.CVE_impact_cvssv3.bm !== 'undefined' ){
 		    		if (item.CVE_impact.CVE_impact_cvssv3.bm.score > 4){
-		    			//console.log('Risk');
-		    			console.log('Riesgo: ', JSON.stringify(json1.CVE_Items[itera]));
-		    			//console.log('Riesgo:', JSON.stringify(item.CVE_impact.CVE_impact_cvssv3.bm.score));
+		    			addResults.push(json1.CVE_Items[itera]); 
 		    		}     	
 		    	}
 		    }
+     	}     	
+    }   
+}
 
+var resultadoFinal = JSON.stringify(addResults);
+console.log(resultadoFinal);
+
+// FILTRO DE FABRICANTE
 		         /*
 		         if ( typeof item.CVE_affects.CVE_vendor.CVE_vendor_data[0] !== 'undefined' ){
 		         	var corps = ['microsoft','linux','oracle','apache','','redhat'];
@@ -86,39 +80,8 @@ for (var item of json1.CVE_Items) {
 		         	}    	     	
 		         }
 		         */
-		     
-		      
 
-		    /*
-     		var substring = "Invalid";
-     		if (howFar.indexOf(substring) > -1){
-     			console.log('2 ' + howFar);
-			}
-     		*/
-
-     		//if (publishDate.fromNow(true) )
-     		//'months' 'invalid'
-     		/*
-			var futureMonth = moment(publishDate).add(1, 'M');
-			var futureMonthEnd = moment(futureMonth).endOf('month');
-			*/
-			/*
-			if(publishDate.date() != futureMonth.date() && futureMonth.isSame(futureMonthEnd.format('YYYY-MM-DD'))) {
-			    futureMonth = futureMonth.add(1, 'd');
-			}
-			*/
-     	}     	
-    }   
-}
-/*
-for(var exKey in obj) {
-      console.log("key:"+exKey+", value:"+exjson[exKey]);
- }
- 
- 
-
-console.log(obj);
-*/
+// PSEUDOCODIGO
 /*
 //Cargo archivo de json a variable, cargar solamente array que nos interesa: CVE_Items
 //Para cada objeto del array solamente dejar los de riesgo CVSSv3 mayores a 4  //CVE_impact > CVE_impact_cvssv3 > score
@@ -127,13 +90,10 @@ console.log(obj);
 
 //Variable json con resultados finales para mostrar en tabla html
 /******************************
-Pseudocodigo: Descargar json desde https://static.nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-modified.json.zip
+Descargar json desde https://static.nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-modified.json.zip
 Descomprimir
 Filtrar solo los de riesgo cvss mayor a 4 - CVE_impact - CVE_impact_cvssv3 - score
 Filtrar los del último mes - CVE_references - CVE_reference_data(0) - publish_date
 Filtrar los que podrian impactar en sistemas del cliente
 Mostrar resultados en tabla html
-
-Agregar aplicaciones a la chdm !
-Rodrigo va a tomar lista de aplicaciones utilizadas en Sistar para luego hacer grep contra la lista de vulnerabilidades.
 *******************************/
